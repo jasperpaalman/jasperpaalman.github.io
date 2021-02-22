afdd5248fec589356a3be1f135f39b6b5d626f94
jasperpaalman <jasper_paalman@hotmail.com>
2021-02-22 17:19:41 +0100

    Add margins

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import About from '../components/sections/About';
-import Eredivisie from '../components/sections/Eredivisie';
+import React from "react";
+import About from "../components/sections/About";
+import Eredivisie from "../components/sections/Eredivisie";
 
 const Home = () => (
-    <div
-        style={{
-            'margin-top': '32pt',
-            'margin-right': '10pt',
-            'margin-left': '10pt',
-        }}
-    >
-        <About />
-        <Eredivisie />
-    </div>
+  <div
+    style={{
+      "margin-top": "32pt",
+      "margin-right": "5pt",
+      "margin-left": "5pt",
+    }}
+  >
+    <About />
+    <Eredivisie />
+  </div>
 );
 
 export default Home;
