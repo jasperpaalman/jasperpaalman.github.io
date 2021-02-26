22224bf22abb6b7f46581b3edb5bc49d8ea1a6c3
jasperpaalman <jasper_paalman@hotmail.com>
2021-02-26 16:46:05 +0100

    extend theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,6 @@
+export const lightBlue = '#51d5ff';
+export const darkGreen = '#345A66';
+
 export const getShadow = (height, fromTop = false) => `${fromTop ? 0 : height * 0.7}pt ${height}pt ${height * 4}pt rgba(0,0,0,.25)`;
 
 export const headerFont = 'roboto, sans-serif';
