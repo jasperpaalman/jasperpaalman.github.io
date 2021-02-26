520601109aee9de8bf1d53e55a4300cfd6adecb3
jasperpaalman <jasper_paalman@hotmail.com>
2021-02-26 16:46:54 +0100

    block for separating sections

---
+++ src/components/elements/block.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BasicBlock = () => (
+    <hr
+        style={{
+            backgroundColor: '#51D5FF',
+            color: '#51D5FF',
+            height: '5pt',
+            opacity: 0.3,
+        }}
+    />
+);
+
+export default BasicBlock;
