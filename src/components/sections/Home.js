fcc7ad8042870e86fdd3cc4ba768ce81626074e0
jasperpaalman <jasper_paalman@hotmail.com>
2021-03-01 10:36:08 +0100

    other

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,25 +1,25 @@
-import React from "react";
-import * as PropTypes from "prop-types";
-import { Visibility } from "semantic-ui-react";
-import BannerSVG from "../../assets/image/home_banner.svg";
-import "./Home.scss";
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import { Visibility } from 'semantic-ui-react';
+import BannerSVG from '../../assets/image/home_banner.svg';
+import './Home.scss';
 
 const Home = ({ onHeaderPassed, onHeaderVisible }) => (
-  <div id="home">
-    <Visibility
-      onBottomPassed={onHeaderPassed}
-      onBottomVisible={onHeaderVisible}
-      once={false}
-    />
-    <div id="banner">
-      <span dangerouslySetInnerHTML={{ __html: BannerSVG }} />
+    <div id="home">
+        <Visibility
+            onBottomPassed={onHeaderPassed}
+            onBottomVisible={onHeaderVisible}
+            once={false}
+        />
+        <div id="banner">
+            <span dangerouslySetInnerHTML={{ __html: BannerSVG }} />
+        </div>
     </div>
-  </div>
 );
 
 Home.propTypes = {
-  onHeaderPassed: PropTypes.any.isRequired,
-  onHeaderVisible: PropTypes.any.isRequired,
+    onHeaderPassed: PropTypes.any.isRequired,
+    onHeaderVisible: PropTypes.any.isRequired,
 };
 
 export default Home;
