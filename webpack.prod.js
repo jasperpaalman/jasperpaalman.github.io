287215c1536496d24dcb442d5851af608b840075
jasperpaalman <jasper_paalman@hotmail.com>
2021-03-01 11:41:39 +0100

    fix icons by unsplitting css and js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -20,8 +20,8 @@ module.exports = merge(common, {
         test: /\.(css|scss)$/,
         use: [
           {
-            // 3. Extract CSS into files
-            loader: MiniCssExtractPlugin.loader,
+            // 3. Inject styles into DOM
+            loader: "style-loader",
           },
           {
             // 2. Translates CSS into CommonJS
@@ -47,9 +47,9 @@ module.exports = merge(common, {
     ],
   },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "static/css/[name].[fullhash].css",
-    }),
-  ],
+  // plugins: [
+  //   new MiniCssExtractPlugin({
+  //     filename: "static/css/[name].[fullhash].css",
+  //   }),
+  // ],
 });
